8c756cdc520f1ad6be9f447c9751827309edca1b
Elvis Oliveira <elvis.olv@gmail.com>
2021-03-28 13:30:09 -0300

    Add podium as txt file

--- typespeed.py
+++ typespeed.py
@@ -5,12 +5,15 @@ words = ["one", "two", "three"]
 name = ""
 
 def play():
-    global words
+    global words, name
     word = words[randint(0, len(words) - 1)]
     print(f"Type the word: {word}")
     response = input()
     if response == word:
         print("Correct!")
+        podium = open("podium.txt","a")
+        print(name, file=podium)
+        podium.close()
     else:
         print("Wrong!")
     main()
@@ -21,8 +24,12 @@ def config():
 
 def main():
     global name
+
+    # Ask for player name
     if(len(name.strip()) == 0):
         name = input("Whats your name:\n")
+
+    # Menu
     response = input(f"Hello {name}\n1 - Play\n2 - Change configurations\n3 - Score\n4 - Exit\n")
     if(response.strip() == "1"):
         play()
