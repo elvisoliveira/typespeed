005f657f4c8de7fa87a853875ca07695db14e5d3
Elvis Oliveira <elvis.olv@gmail.com>
2021-03-28 13:06:00 -0300

    Add menu score, ask player name

--- typespeed.py
+++ typespeed.py
@@ -2,6 +2,7 @@ import sys
 from random import randint
 
 words = ["one", "two", "three"]
+name = ""
 
 def play():
     global words
@@ -15,15 +16,19 @@ def play():
     main()
 
 def config():
-    print("Configure the game")
+    print("Configure the game\n")
+    main()
 
 def main():
-    response = input("1 - Play\n2 - Change configurations\n3 - Exit\n")
+    global name
+    if(len(name.strip()) == 0):
+        name = input("Whats your name:\n")
+    response = input(f"Hello {name}\n1 - Play\n2 - Change configurations\n3 - Score\n4 - Exit\n")
     if(response.strip() == "1"):
         play()
     elif(response.strip() == "2"):
         config()
-    elif(response.strip() == "3"):
+    elif(response.strip() == "4"):
         sys.exit()
     else:
         main()
