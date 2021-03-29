6f3c7e662c4aa982fde5caabbe441bdc569d3f57
Elvis Oliveira <elvis.olv@gmail.com>
2021-03-29 00:37:50 -0300

    Update typespeed.py

--- typespeed.py
+++ typespeed.py
@@ -1,41 +1,54 @@
 import sys
+import time
 from random import randint
 
 words = ["one", "two", "three"]
 name = ""
+nameList = []
+totalPlayers = 3
+totalWords = 1
 
 def play():
     global words, name
+
+    # Ask for players name
+    count = 0
+    while count < totalPlayers:
+        nameList.append(input("Whats your name:\n"))
+        count += 1
+
     word = words[randint(0, len(words) - 1)]
     print(f"Type the word: {word}")
+
+    speed = time.time();
     response = input()
     if response == word:
-        print("Correct!")
-        podium = open("podium.txt","a")
-        print(name, file=podium)
-        podium.close()
+        print("Correct!\n")
     else:
-        print("Wrong!")
+        print("Wrong!\n")
+    total_secs = round(time.time() - speed)
+    print(f"Seu tempo foi de {total_secs}")
     main()
 
 def config():
-    print("Configure the game\n")
+    global totalPlayers, totalWords
+    print("Configure the game")
+    response = input(f"1 - Quantos players? [{totalPlayers}]\n2 - Quantas palavras? [{totalWords}]\n")
+    if(response == "1"):
+        totalPlayers = input("Digite um valor entre 2 e 5\n")
+    if(response == "2"):
+        totalWords = input("Digite um valor entre 2 e 5\n")
     main()
 
 def main():
     global name
-
-    # Ask for player name
-    if(len(name.strip()) == 0):
-        name = input("Whats your name:\n")
-
     # Menu
-    response = input(f"Hello {name}\n1 - Play\n2 - Change configurations\n3 - Score\n4 - Exit\n")
+    response = input(f"Hello {name}\n1 - Play\n2 - Change configurations\n3 - Exit\n")
     if(response.strip() == "1"):
         play()
     elif(response.strip() == "2"):
         config()
-    elif(response.strip() == "4"):
+    elif(response.strip() == "3"):
         sys.exit()
     else:
         main()
