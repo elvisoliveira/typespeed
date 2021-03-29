90e69c266e2b6ce521674d02af6b6a744904949a
Elvis Oliveira <elvis.olv@gmail.com>
2021-03-29 19:23:42 -0300

    Update typespeed.py

--- typespeed.py
+++ typespeed.py
@@ -2,32 +2,37 @@ import sys
 import time
 from random import randint
 
-words = ["one", "two", "three"]
-name = ""
+words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
 nameList = []
-totalPlayers = 3
-totalWords = 1
+totalPlayers = 2
+totalWords = 5
 
 def play():
-    global words, name
+    global words
 
-    # Ask for players name
     count = 0
     while count < totalPlayers:
-        nameList.append(input("Whats your name:\n"))
+        nameList.append(input(f"Whats your name player {count}\n"))
         count += 1
 
-    word = words[randint(0, len(words) - 1)]
-    print(f"Type the word: {word}")
+    for name in nameList:
+        totalTime = 0
+        countwords = 0
+        while countwords < totalWords:
+            word = words[randint(0, len(words) - 1)]
+            print(f"{name}, type the word: {word}")
+            speed = time.time();
+            response = input()
+            if response == word:
+                print("Correct!")
+            else:
+                print("Wrong!")
+            total_speed = round(time.time() - speed)
+            print(f"Your time {total_speed} seconds")
+            countwords = countwords + 1
+            totalTime = totalTime + total_speed
+        print(f"{name}, your total time is {totalTime} seconds")
 
-    speed = time.time();
-    response = input()
-    if response == word:
-        print("Correct!\n")
-    else:
-        print("Wrong!\n")
-    total_secs = round(time.time() - speed)
-    print(f"Seu tempo foi de {total_secs}")
     main()
 
 def config():
@@ -35,15 +40,21 @@ def config():
     print("Configure the game")
     response = input(f"1 - Quantos players? [{totalPlayers}]\n2 - Quantas palavras? [{totalWords}]\n")
     if(response == "1"):
-        totalPlayers = input("Digite um valor entre 2 e 5\n")
+        totalPlayersTemp = input("Digite um valor entre 2 e 5\n ")
+        if totalPlayersTemp < "2" or totalPlayersTemp > "5":
+            print("Quantidade inválida")
+        else:
+            totalPlayers = totalPlayersTemp
     if(response == "2"):
-        totalWords = input("Digite um valor entre 2 e 5\n")
+        totalWordsTemp = input("Digite um valor entre 5 e 10\n")
+        if totalWordsTemp < "5" or totalWordsTemp > "10":
+            print("Quantidade inválida")
+        else:
+            totalWords = totalWordsTemp
     main()
 
 def main():
-    global name
-    # Menu
-    response = input(f"Hello {name}\n1 - Play\n2 - Change configurations\n3 - Exit\n")
+    response = input(f"1 - Play\n2 - Change configurations\n3 - Exit\n")
     if(response.strip() == "1"):
         play()
     elif(response.strip() == "2"):
